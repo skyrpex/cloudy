7eee3bed60347893e29d09ef4e29271bc9bd95f2
Cristian Pallarés <cristian.pallares@virtualtools.com.au>
2022-04-15 12:57:05 +0200

    wip

--- packages/cdk/src/aws-dynamodb/table.type-test.ts
+++ packages/cdk/src/aws-dynamodb/table.type-test.ts
@@ -9,6 +9,7 @@ import { F } from "ts-toolbelt";
 import { staticTest } from "../static-test.js";
 
 import { ValueType } from "../value-type.js";
+import { IsExact, typeAssert } from "./table.conditional-type-checks.js";
 
 type DynamodbPrimitiveValues =
   | string
@@ -37,12 +38,19 @@ type TypeFromAttributeType<T extends AttributeType> =
     : T extends AttributeType.BINARY
     ? Uint8Array
     : never;
+// type AttributeFromKeyDefinition<T extends KeyDefinition | undefined> =
+//   T extends KeyDefinition
+//     ? {
+//         [name in T["name"]]: TypeFromAttributeType<T["type"]>;
+//       }
+//     : {};
+
+type RemoveUndefined<T> = T extends undefined ? never : T;
+type AttributeFromKeyDefinition3Base<T extends KeyDefinition> = {
+  [name in T["name"]]: TypeFromAttributeType<T["type"]>;
+};
 type AttributeFromKeyDefinition<T extends KeyDefinition | undefined> =
-  T extends KeyDefinition
-    ? {
-        [name in T["name"]]: TypeFromAttributeType<T["type"]>;
-      }
-    : {};
+  AttributeFromKeyDefinition3Base<RemoveUndefined<T>>;
 
 type AccessPattern<
   PartitionKey extends KeyDefinition,
@@ -148,61 +156,109 @@ type Materialize<T extends TableProperties<any, any, any, any>> = {
     : never;
 };
 
-type p1 = Materialize<
-  TableProperties<
-    { name: "id"; type: AttributeType.STRING },
-    undefined,
-    {
-      id: string;
-    },
-    undefined
-  >
->;
-type p2 = Materialize<
-  TableProperties<
+staticTest(() => {
+  type p1 = Materialize<
+    TableProperties<
+      { name: "id"; type: AttributeType.STRING },
+      undefined,
+      { id: string },
+      undefined
+    >
+  >;
+  typeAssert<
+    IsExact<
+      p1,
+      {
+        partitionKey: "id";
+        sortKey: never;
+        itemType: { id: string };
+        stream: never;
+      }
+    >
+  >(true);
+
+  type p2 = Materialize<
+    TableProperties<
+      { name: "id"; type: AttributeType.STRING },
+      { name: "sk"; type: AttributeType.STRING },
+      { id: string; sk: string },
+      undefined
+    >
+  >;
+  typeAssert<
+    IsExact<
+      p2,
+      {
+        partitionKey: "id";
+        sortKey: "sk";
+        itemType: { id: string; sk: string };
+        stream: never;
+      }
+    >
+  >(true);
+
+  type p3 = Materialize<
+    TableProperties<
+      { name: "id"; type: AttributeType.STRING },
+      { name: "sk"; type: AttributeType.NUMBER },
+      { id: string; sk: number; age: number },
+      undefined
+    >
+  >;
+  typeAssert<
+    IsExact<
+      p3,
+      {
+        partitionKey: "id";
+        sortKey: "sk";
+        itemType: { id: string; sk: number; age: number };
+        stream: never;
+      }
+    >
+  >(true);
+
+  type p4 = TableProperties<
     { name: "id"; type: AttributeType.STRING },
     { name: "sk"; type: AttributeType.STRING },
-    { id: string; sk: string },
+    // @ts-expect-error: sk is missing.
+    { id: string },
     undefined
-  >
->;
-type p3 = Materialize<
-  TableProperties<
+  >;
+  type p5 = TableProperties<
     { name: "id"; type: AttributeType.STRING },
     { name: "sk"; type: AttributeType.STRING },
-    { id: string; sk: string; age: number },
+    // @ts-expect-error: id is missing.
+    {},
     undefined
-  >
->;
-type p4 = TableProperties<
-  { name: "id"; type: AttributeType.STRING },
-  { name: "sk"; type: AttributeType.STRING },
-  // @ts-expect-error: sk is missing.
-  { id: string },
-  undefined
->;
-type p5 = TableProperties<
-  { name: "id"; type: AttributeType.STRING },
-  { name: "sk"; type: AttributeType.STRING },
-  // @ts-expect-error: id is missing.
-  {},
-  undefined
->;
-type p6 = TableProperties<
-  { name: "id"; type: AttributeType.STRING },
-  undefined,
-  // @ts-expect-error: id is missing.
-  {},
-  undefined
->;
-type p7 = Materialize<
-  TableProperties<
+  >;
+  type p6 = TableProperties<
     { name: "id"; type: AttributeType.STRING },
     undefined,
-    undefined,
+    // @ts-expect-error: id is missing.
+    {},
     undefined
-  >
->;
+  >;
+
+  type p7 = Materialize<
+    TableProperties<
+      { name: "id"; type: AttributeType.STRING },
+      undefined,
+      undefined,
+      undefined
+    >
+  >;
+  typeAssert<
+    IsExact<
+      p7,
+      {
+        partitionKey: "id";
+        sortKey: never;
+        itemType: { id: string };
+        stream: never;
+      }
+    >
+  >(true);
+});
 
 type TableName<T extends MaterializedTableProperties> = OpaqueType<string, T>;
 
@@ -458,93 +514,93 @@ staticTest(() => {
 
 // });
 
-staticTest(() => {
-  interface TableProperties2<
-    PartitionKey extends KeyDefinition,
-    SortKey extends KeyDefinition | undefined,
-    ItemType extends AccessPattern<PartitionKey, SortKey> | undefined,
-    Stream extends StreamViewType | undefined = StreamViewType | undefined,
-  > extends TableProps {
-    partitionKey: PartitionKey;
-    sortKey?: SortKey;
-    itemType?: ValueType<ItemType>;
-    stream?: Stream;
-  }
+// staticTest(() => {
+//   interface TableProperties2<
+//     PartitionKey extends KeyDefinition,
+//     SortKey extends KeyDefinition | undefined,
+//     ItemType extends AccessPattern<PartitionKey, SortKey> | undefined,
+//     Stream extends StreamViewType | undefined = StreamViewType | undefined,
+//   > extends TableProps {
+//     partitionKey: PartitionKey;
+//     sortKey?: SortKey;
+//     itemType?: ValueType<ItemType>;
+//     stream?: Stream;
+//   }
 
-  // interface MaterializedTableProperties2<ItemType extends DynamodbItem> {
-  //   partitionKey: keyof ItemType;
-  //   sortKey: keyof ItemType | never;
-  //   // itemType: ItemType;
-  //   stream: StreamViewType | never;
-  // }
+//   // interface MaterializedTableProperties2<ItemType extends DynamodbItem> {
+//   //   partitionKey: keyof ItemType;
+//   //   sortKey: keyof ItemType | never;
+//   //   // itemType: ItemType;
+//   //   stream: StreamViewType | never;
+//   // }
 
-  type Materialize2<T extends TableProperties2<any, any, any>> = {
-    partitionKey: T["partitionKey"]["name"];
-    sortKey: T["sortKey"] extends infer SortKey
-      ? SortKey extends KeyDefinition
-        ? SortKey["name"]
-        : never
-      : never;
-    itemType: ResolveValueType<
-      T["itemType"] extends infer ItemType | undefined
-        ? ItemType
-        : T["itemType"]
-    > extends infer ItemType
-      ? unknown extends ItemType
-        ? AccessPattern<T["partitionKey"], T["sortKey"]>
-        : ItemType extends undefined
-        ? AccessPattern<T["partitionKey"], T["sortKey"]>
-        : ItemType
-      : AccessPattern<T["partitionKey"], T["sortKey"]>;
-    stream: T["stream"] extends infer Stream
-      ? Stream extends StreamViewType
-        ? Stream
-        : never
-      : never;
-  };
+//   type Materialize2<T extends TableProperties2<any, any, any>> = {
+//     partitionKey: T["partitionKey"]["name"];
+//     sortKey: T["sortKey"] extends infer SortKey
+//       ? SortKey extends KeyDefinition
+//         ? SortKey["name"]
+//         : never
+//       : never;
+//     itemType: ResolveValueType<
+//       T["itemType"] extends infer ItemType | undefined
+//         ? ItemType
+//         : T["itemType"]
+//     > extends infer ItemType
+//       ? unknown extends ItemType
+//         ? AccessPattern<T["partitionKey"], T["sortKey"]>
+//         : ItemType extends undefined
+//         ? AccessPattern<T["partitionKey"], T["sortKey"]>
+//         : ItemType
+//       : AccessPattern<T["partitionKey"], T["sortKey"]>;
+//     stream: T["stream"] extends infer Stream
+//       ? Stream extends StreamViewType
+//         ? Stream
+//         : never
+//       : never;
+//   };
 
-  type t1 = Materialize2<
-    TableProperties2<
-      { name: "id"; type: AttributeType.STRING },
-      undefined,
-      undefined,
-      undefined
-    >
-  >;
-  type t2 = Materialize2<
-    TableProperties2<
-      { name: "id"; type: AttributeType.STRING },
-      { name: "sk"; type: AttributeType.STRING },
-      undefined,
-      undefined
-    >
-  >;
-  type t3 = Materialize2<
-    TableProperties2<
-      { name: "id"; type: AttributeType.STRING },
-      { name: "sk"; type: AttributeType.STRING },
-      undefined,
-      StreamViewType.NEW_IMAGE
-    >
-  >;
-  const t3: t3 = {
-    itemType: {
-      id: "",
-      // sk: 1,
-    },
-    partitionKey: "id",
-    sortKey: "sk",
-    stream: StreamViewType.NEW_IMAGE,
-  };
-  type t4 = Materialize2<
-    TableProperties2<
-      { name: "id"; type: AttributeType.STRING },
-      { name: "sk"; type: AttributeType.STRING },
-      { id: string; sk: string; age: number },
-      StreamViewType.NEW_IMAGE
-    >
-  >;
-});
+//   type t1 = Materialize2<
+//     TableProperties2<
+//       { name: "id"; type: AttributeType.STRING },
+//       undefined,
+//       undefined,
+//       undefined
+//     >
+//   >;
+//   type t2 = Materialize2<
+//     TableProperties2<
+//       { name: "id"; type: AttributeType.STRING },
+//       { name: "sk"; type: AttributeType.STRING },
+//       undefined,
+//       undefined
+//     >
+//   >;
+//   type t3 = Materialize2<
+//     TableProperties2<
+//       { name: "id"; type: AttributeType.STRING },
+//       { name: "sk"; type: AttributeType.STRING },
+//       undefined,
+//       StreamViewType.NEW_IMAGE
+//     >
+//   >;
+//   const t3: t3 = {
+//     itemType: {
+//       id: "",
+//       // sk: 1,
+//     },
+//     partitionKey: "id",
+//     sortKey: "sk",
+//     stream: StreamViewType.NEW_IMAGE,
+//   };
+//   type t4 = Materialize2<
+//     TableProperties2<
+//       { name: "id"; type: AttributeType.STRING },
+//       { name: "sk"; type: AttributeType.STRING },
+//       { id: string; sk: string; age: number },
+//       StreamViewType.NEW_IMAGE
+//     >
+//   >;
+// });
 
 staticTest(() => {
   type Event<T extends MaterializedTableProperties> = T["stream"] extends never
@@ -552,44 +608,221 @@ staticTest(() => {
     : {
         item: T["itemType"];
       };
-  type t1 = Event<
-    Materialize<
-      TableProperties<
-        { name: "id"; type: AttributeType.STRING },
-        undefined,
-        undefined,
-        undefined
+  typeAssert<
+    IsExact<
+      never,
+      Event<
+        Materialize<
+          TableProperties<
+            { name: "id"; type: AttributeType.STRING },
+            undefined,
+            undefined,
+            undefined
+          >
+        >
       >
     >
-  >;
-  type t2 = Event<
-    Materialize<
-      TableProperties<
-        { name: "id"; type: AttributeType.STRING },
-        undefined,
-        undefined,
-        StreamViewType.NEW_IMAGE
+  >(true);
+
+  typeAssert<
+    IsExact<
+      { item: { id: string } },
+      Event<
+        Materialize<
+          TableProperties<
+            { name: "id"; type: AttributeType.STRING },
+            undefined,
+            undefined,
+            StreamViewType.NEW_IMAGE
+          >
+        >
       >
     >
-  >;
-  type t3 = Event<
-    Materialize<
-      TableProperties<
-        { name: "id"; type: AttributeType.STRING },
-        { name: "id2"; type: AttributeType.STRING },
-        undefined,
-        StreamViewType.NEW_IMAGE
+  >(true);
+
+  typeAssert<
+    IsExact<
+      { item: { pk: string; sk: string } },
+      Event<
+        Materialize<
+          TableProperties<
+            { name: "pk"; type: AttributeType.STRING },
+            { name: "sk"; type: AttributeType.STRING },
+            undefined,
+            StreamViewType.NEW_IMAGE
+          >
+        >
       >
     >
-  >;
-  type t4 = Event<
-    Materialize<
-      TableProperties<
-        { name: "id"; type: AttributeType.STRING },
-        { name: "id2"; type: AttributeType.STRING },
-        { id: "a"; id2: string },
-        StreamViewType.NEW_IMAGE
+  >(true);
+
+  typeAssert<
+    IsExact<
+      { item: { pk: "pk"; sk: "sk"; age: number } },
+      Event<
+        Materialize<
+          TableProperties<
+            { name: "pk"; type: AttributeType.STRING },
+            { name: "sk"; type: AttributeType.STRING },
+            { pk: "pk"; sk: "sk"; age: number },
+            StreamViewType.NEW_IMAGE
+          >
+        >
       >
     >
-  >;
+  >(true);
+});
+
+staticTest(() => {
+  typeAssert<
+    IsExact<
+      Materialize<
+        TableProperties<
+          { name: "pk"; type: AttributeType.STRING },
+          undefined,
+          undefined,
+          undefined
+        >
+      >,
+      {
+        partitionKey: "pk";
+        sortKey: never;
+        itemType: {
+          pk: string;
+        };
+        stream: never;
+      }
+    >
+  >(true);
+
+  // // itemType: ResolveValueType<
+  // //   T["itemType"] extends infer ItemType | undefined ? ItemType : T["itemType"]
+  // // > extends infer ItemType
+  // //   ? unknown extends ItemType
+  // //     ? AccessPattern<T["partitionKey"], T["sortKey"]>
+  // //     : ItemType extends undefined
+  // //     ? AccessPattern<T["partitionKey"], T["sortKey"]>
+  // //     : ItemType
+  // //   : AccessPattern<T["partitionKey"], T["sortKey"]>;
+
+  // type AttributeFromKeyDefinition2<T extends KeyDefinition | undefined> =
+  //   T extends KeyDefinition
+  //     ? {
+  //         [name in T["name"]]: TypeFromAttributeType<T["type"]>;
+  //       }
+  //     : {};
+  // typeAssert<
+  //   IsExact<
+  //     AttributeFromKeyDefinition2<{
+  //       name: "pk";
+  //       type: AttributeType.STRING;
+  //     }>,
+  //     {
+  //       pk: string;
+  //     }
+  //   >
+  // >(true);
+  // typeAssert<
+  //   IsExact<
+  //     AttributeFromKeyDefinition2<{
+  //       name: "pk";
+  //       type: AttributeType.NUMBER;
+  //     }>,
+  //     {
+  //       pk: number | bigint;
+  //     }
+  //   >
+  // >(true);
+  // typeAssert<IsExact<AttributeFromKeyDefinition2<undefined>, {}>>(true);
+
+  // // type MatItem<T extends TableProperties<any, any, any, any>> = AccessPattern<
+  // //   T["partitionKey"],
+  // //   T["sortKey"]
+  // // >;
+
+  // type RemoveUndefined<T> = T extends undefined ? never : T;
+  // type AttributeFromKeyDefinition3Base<T extends KeyDefinition> = {
+  //   [name in T["name"]]: TypeFromAttributeType<T["type"]>;
+  // };
+  // type AttributeFromKeyDefinition3<T extends KeyDefinition | undefined> =
+  //   AttributeFromKeyDefinition3Base<RemoveUndefined<T>>;
+
+  // type MatItem<
+  //   T extends TableProperties<
+  //     KeyDefinition,
+  //     KeyDefinition | undefined,
+  //     any,
+  //     any
+  //   >,
+  //   // > = AttributeFromKeyDefinition2<T["partitionKey"]> &
+  //   //   (T["sortKey"] extends infer SortKey
+  //   //     ? SortKey extends KeyDefinition
+  //   //       ? AttributeFromKeyDefinition2<SortKey>
+  //   //       : never
+  //   //     : {});
+  // > = AttributeFromKeyDefinition3<T["partitionKey"]> &
+  //   AttributeFromKeyDefinition3<T["sortKey"]>;
+
+  // type AccessPattern<
+  //   PartitionKey extends KeyDefinition,
+  //   SortKey extends KeyDefinition | undefined,
+  // > = DynamodbItem &
+  //   AttributeFromKeyDefinition3<PartitionKey> &
+  //   AttributeFromKeyDefinition3<SortKey>;
+  // type r1 = AccessPattern<
+  //   { name: "pk"; type: AttributeType.STRING },
+  //   undefined
+  // >;
+
+  // type r = MatItem<
+  //   TableProperties<
+  //     { name: "pk"; type: AttributeType.STRING },
+  //     // { name: "sk"; type: AttributeType.STRING },
+  //     undefined,
+  //     undefined,
+  //     undefined
+  //   >
+  // >;
+  // typeAssert<
+  //   IsExact<
+  //     r,
+  //     {
+  //       pk: string;
+  //       // sk: string;
+  //     }
+  //   >
+  // >(true);
+  // typeAssert<
+  //   IsExact<
+  //     MatItem<
+  //       TableProperties<
+  //         { name: "pk"; type: AttributeType.STRING },
+  //         // { name: "sk"; type: AttributeType.STRING },
+  //         undefined,
+  //         undefined,
+  //         undefined
+  //       >
+  //     >,
+  //     {
+  //       pk: string;
+  //       // sk: string;
+  //     }
+  //   >
+  // >(true);
+  // typeAssert<
+  //   IsExact<
+  //     MatItem<
+  //       TableProperties<
+  //         { name: "pk"; type: AttributeType.STRING },
+  //         { name: "sk"; type: AttributeType.STRING },
+  //         undefined,
+  //         undefined
+  //       >
+  //     >,
+  //     {
+  //       pk: string;
+  //       sk: string;
+  //     }
+  //   >
+  // >(true);
 });
