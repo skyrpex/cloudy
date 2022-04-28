a019016d029a5aaf6a05aa935ead4af647e87af6
Cristian Pallarés <cristian.pallares@virtualtools.com.au>
2022-04-28 21:52:24 +0200

    feat(aws-lambda): cloudy IFunction extends now from aws IFunction (#28)

--- src/aws-lambda/function-base.ts
+++ src/aws-lambda/function-base.ts
@@ -1,6 +1,8 @@
+import { IFunction as IBaseFunction } from "aws-cdk-lib/aws-lambda";
 import { IEventSource } from "./event-source.js";
 
-export interface IFunction<InputType, OutputType> {
+export interface IFunction<InputType = any, OutputType = any>
+  extends IBaseFunction {
   /**
    * Adds an event source to this function.
    */
